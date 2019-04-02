bb1f109a8f3e085450be9764c09fe2d6bec6d2ef
fnoquiq <gabriel_t_ana@hotmail.com>
2019-04-02 02:12:24 -0300

    Adicionado funcionalidade para abrir camera
    Adicionado funcionalidade que instancia uma janela capturando as imagens da webcam
    Alterado .ignore, agora o git não subirá arquivos de configurações do pycharm, no caso a pasta .idea

--- average_speed_radar/webcam_integration/webcam_integration.py
+++ average_speed_radar/webcam_integration/webcam_integration.py
@@ -1,1 +1,14 @@
-@ToDo
+import cv2
+
+video = cv2.VideoCapture(0)
+
+while True:
+    conectado,  frame = video.read()
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
